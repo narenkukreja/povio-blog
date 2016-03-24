5430ddcad1540e7ffb05813e17e4057600275a6d
Naren Kukreja <narenkukreja@gmail.com>
2016-03-24 14:27:52 -0400

    Add few posts to the index page for testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,29 @@
 class PostsController < ApplicationController
 
   def index
-
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
-    
+
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  #private method to allow the post to permit and save data for the title and description
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 
 end
